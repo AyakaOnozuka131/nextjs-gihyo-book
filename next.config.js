9e0e32e87f7409698e42f69a70f52b299e56f4a3
AyakaOnozuka131 <c_yu_again@icloud.com>
2023-01-29 17:29:22 +0900

    setting : Add styled-components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: () => {
+    let compilerConfig = {
+      styledComponents: true,
+    };
+    return compilerConfig;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
