8af56441e4941f3082d04d12d76f7e7eaac6219f
AyakaOnozuka131 <c_yu_again@icloud.com>
2023-01-29 18:29:10 +0900

    change : format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-const inter = Inter({ subsets: ['latin'] });
+import { Inter } from '@next/font/google'
+import Head from 'next/head'
+import Image from 'next/image'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,5 +14,5 @@ export default function Home() {
       </Head>
       <p>こんにちは</p>
     </>
-  );
+  )
 }
